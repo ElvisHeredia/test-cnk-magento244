76387a8b86d53c210f55b9295acb6992d645abc5
Elvis Heredia <elvis.heredia@conekta.com>
2022-06-09 11:21:24 -0500

    fix: fix function in class class Logger extends \Monolog\Logger

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -8,12 +8,12 @@ use Magento\Framework\App\ObjectManager;
 class Logger extends \Monolog\Logger
 {
     /**
-     * @param $level
-     * @param $message
+     * @param int $level
+     * @param string $message
      * @param array $context
      * @return bool
      */
-    public function addRecord($level, $message, array $context = []): bool
+    public function addRecord(int $level, string $message, array $context = []): bool
     {
         $objectManager = ObjectManager::getInstance();
         $conektaHelper = $objectManager->create(Data::class);
