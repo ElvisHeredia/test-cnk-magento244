58112f2135f8c19a24ecb2b3e92a93e2e8c09d4a
Elvis Heredia <elvis.heredia@conekta.com>
2022-06-10 14:17:53 -0500

    fix: logger

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -9,6 +9,13 @@ use Monolog\Logger as MonoLogger;
 class Logger
 {
     /**
+     * @param MonoLogger $monolog
+     */
+    public function __construct(private MonoLogger $monolog)
+    {
+    }
+
+    /**
      * @param int $level
      * @param string $message
      * @param array $context
@@ -20,9 +27,39 @@ class Logger
         $conektaHelper = $objectManager->create(Data::class);
 
         if ((int)$conektaHelper->getConfigData('conekta/conekta_global', 'debug')) {
-            return MonoLogger::addRecord($level, $message, $context);
+            return $this->monolog->addRecord($level, $message, $context);
         }
 
         return true;
+    }
+
+    /**
+     * @param string $string
+     * @param array $customerRequest
+     * @return void
+     */
+    public function info(string $string, array $customerRequest): void
+    {
+        $this->monolog->info($string, $customerRequest);
+    }
+
+    /**
+     * @param string $string
+     * @param array $array
+     * @return void
+     */
+    public function error(string $string, array $array): void
+    {
+        $this->monolog->error($string, $array);
+    }
+
+    /**
+     * @param string $message
+     * @param array $array
+     * @return void
+     */
+    public function debug(string $message, array $array = []): void
+    {
+        $this->monolog->debug($message, $array);
     }
 }
